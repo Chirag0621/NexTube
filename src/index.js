7b14337b4d457b37ba39fc892324f8540744ea96
Chirag Bhadani <kumarchirag978@gmail.com>
2026-05-09 23:28:28 +0530

    Connected Database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("Hello from Chirag")
+import { app } from "./app.js";
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: './.env'
+})
+const PORT = process.env.PORT || 7000
+
+connectDB()
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on the port: ${PORT}`)
+  })
+})
+.catch((err) => {
+  console.log("MongoDB connection error", err)
+})
